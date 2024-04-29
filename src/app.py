ac3bc497ac0b6037dc54d374491d0c22b0df1710
pauloalmeida46 <162117908+pauloalmeida46@users.noreply.github.com>
2024-04-29 08:01:44 -0300

    atualização do app.py com nova página do site

--- src/app.py
+++ src/app.py
@@ -5,8 +5,13 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
+
 @app.route("/index.html")
 def index():
     return render_template('index.html')
+
+@app.route("/graficos.html")
+def graficos():
+    return render_template("graficos.html")
 
 app.run(debug=True)
