c911229651342dc7cef74f2def8b01dd9e29af08
EduardoKuwahara <162118889+EduardoKuwahara@users.noreply.github.com>
2024-05-03 22:57:45 -0300

    Atualizações e Integração com Banco de Dados
    Atualização do requirements.txt e do README.MD.
    .env com as credências de acesso para o MySQL
    app.py com a integração do Banco de Dados e a aplicação Flask

--- src/app.py
+++ src/app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, jsonify
+from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+load_dotenv()
+
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = os.getenv("DB_HOST")
+app.config['MYSQL_USER'] = os.getenv("DB_USER")
+app.config['MYSQL_PASSWORD'] = os.getenv("DB_PASSWORD")
+app.config['MYSQL_DB'] = os.getenv("DB_NAME")
+
+mysql = MySQL (app)
+
 
 @app.route("/")
 def home():
@@ -12,6 +25,33 @@ def index():
 
 @app.route("/graficos.html")
 def graficos():
-    return render_template("graficos.html")
+     return render_template("graficos.html")
+    
+@app.route("/api/dados", methods=["GET"])
+def get_dados():
+    if mysql.connection:
+        try:
+            cursor = mysql.connection.cursor()
+            cursor.execute("SELECT DiaSemana, Dia_Mes_Ano, Hora, Temperatura, UmidadeSolo, UmidadeAmbiente, VolumeAgua FROM dados")
+            resultados = cursor.fetchall()
 
-app.run(debug=True)
+            dados = [{
+                'DiaSemana': row[0],
+                'Data': row[1],  # Colunas do banco de dados
+                'Hora': row[2],
+                'Temperatura': row[3],
+                'UmidadeSolo': row[4],
+                'UmidadeAmbiente': row[5],
+                'VolumeAgua': row[6],
+            } for row in resultados]
+            return jsonify(dados)  # Retorna dados como JSON
+        finally:
+            cursor.close()
+    else:
+        return jsonify({'error': 'Erro ao tentar estabelecer conexão com o Banco de Dados'}), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
+
+
