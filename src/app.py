7f74205a6b64f8cb6a827ae12e0587587f440bce
jofran2001 <119539664+jofran2001@users.noreply.github.com>
2024-05-25 14:11:14 -0300

    app.py atualizado
    app.py atualizado de acordo com as necessidades e exigências do Docker

--- src/app.py
+++ src/app.py
@@ -1,91 +1,108 @@
-import os
-from flask import Flask, render_template, jsonify, request, redirect
-from flask_mysqldb import MySQL
+from flask import Flask, render_template, jsonify, request
+import mysql.connector
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = "localhost"
-app.config['MYSQL_USER'] = "root" #ALTERAR O NOME DE USUÁRIO DO MYSQL
-app.config['MYSQL_PASSWORD'] = "anuubiss1" #ALTERAR PARA A SUA SENHA DO MYSQL
-app.config['MYSQL_DB'] = "dadosestufa"
+db_config = {
+    'user': 'usuario', #ALTERAR PARA SEU USUÁRIO NO MYSQL
+    'password': 'senha', #ALTERAR PARA A SUA SENHA NO MYSQL
+    'host': 'db', #ALTERAR PARA O SEU NOME DE USUÁRIO NO MYSQL
+}
 
-mysql = MySQL (app)
+conn = mysql.connector.connect(**db_config)
 
 @app.route("/")
 def home():
-    cursor = mysql.connection.cursor()
-    query = "SELECT Dia_Mes_Ano, Hora FROM dados ORDER BY dado_cod DESC LIMIT 1;"
-    cursor.execute(query)
-    resultado = cursor.fetchone()
-    print("Resultado da consulta:", resultado)
-    ultima_data = resultado[0]
-    ultima_hora = resultado[1]
-    cursor.close()
-    return render_template("index.html",ultima_data=ultima_data, ultima_hora=ultima_hora )
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+        cursor.execute("use DadosEstufa;")
+        query = "SELECT Dia_Mes_Ano, Hora FROM dados ORDER BY dado_cod DESC LIMIT 1;"
+        cursor.execute(query)
+        resultado = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        
+        if resultado:
+            ultima_data = resultado[0]
+            ultima_hora = resultado[1]
+        else:
+            ultima_data = None
+            ultima_hora = None
+        return render_template("index.html", ultima_data=ultima_data, ultima_hora=ultima_hora)
+    except mysql.connector.Error as err:
+        return f"Erro: {err}", 500
 
-    
 @app.route("/api/dados", methods=["GET"])
 def get_dados():
-    if mysql.connection:
-        try:
-            cursor = mysql.connection.cursor()
-            cursor.execute("SELECT DiaSemana, Dia_Mes_Ano, Hora, Temperatura, UmidadeSolo, UmidadeAmbiente, VolumeAgua FROM dados")
-            resultados = cursor.fetchall()
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+        cursor.execute("use DadosEstufa;")
+        cursor.execute("SELECT DiaSemana, Dia_Mes_Ano, Hora, Temperatura, UmidadeSolo, UmidadeAmbiente, VolumeAgua FROM dados")
+        resultados = cursor.fetchall()
+        cursor.close()
+        conn.close()
 
-            dados = [{
-                'DiaSemana': row[0],
-                'Data': row[1],  # Colunas do banco de dados
-                'Hora': row[2],
-                'Temperatura': row[3],
-                'UmidadeSolo': row[4],
-                'UmidadeAmbiente': row[5],
-                'VolumeAgua': row[6],
-            } for row in resultados]
-            return jsonify(dados)  # Retorna dados como JSON
-        finally:
-            cursor.close()
-    else:
-        return jsonify({'error': 'Erro ao tentar estabelecer conexão com o Banco de Dados'}), 500
+        dados = [{
+            'DiaSemana': row[0],
+            'Data': row[1],  
+            'Hora': row[2],
+            'Temperatura': row[3],
+            'UmidadeSolo': row[4],
+            'UmidadeAmbiente': row[5],
+            'VolumeAgua': row[6],
+        } for row in resultados]
+        return jsonify(dados)
+    except mysql.connector.Error as err:
+        return jsonify({'error': f"Erro: {err}"}), 500
 
 @app.route("/api/medias", methods=["GET"])
 def get_medias():
-    if mysql.connection:
-        try:
-            cursor = mysql.connection.cursor()
-            cursor.execute("SELECT MAX(Dia_Mes_Ano) FROM dados")
-            ultima_data = cursor.fetchone()[0]
-            cursor.execute("SELECT AVG(Temperatura), AVG(UmidadeSolo), AVG(UmidadeAmbiente), AVG(VolumeAgua) FROM dados WHERE Dia_Mes_Ano = %s", (ultima_data,))
-            medias = cursor.fetchone()
-            return jsonify({
-                'TemperaturaMedia': medias[0],
-                'UmidadeSoloMedia': medias[1],
-                'UmidadeAmbienteMedia': medias[2],
-                'VolumeAguaMedia': medias[3],
-            })
-        finally:
-            cursor.close()
-    else:
-        return jsonify({'error': 'Erro ao tentar estabelecer conexão com o Banco de Dados'}), 500
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+        cursor.execute("use DadosEstufa;")
+        cursor.execute("SELECT MAX(Dia_Mes_Ano) FROM dados")
+        ultima_data = cursor.fetchone()[0]
+        cursor.execute("SELECT AVG(Temperatura), AVG(UmidadeSolo), AVG(UmidadeAmbiente), AVG(VolumeAgua) FROM dados WHERE Dia_Mes_Ano = %s", (ultima_data,))
+        medias = cursor.fetchone()
+        cursor.close()
+        conn.close()
 
+        return jsonify({
+            'TemperaturaMedia': medias[0],
+            'UmidadeSoloMedia': medias[1],
+            'UmidadeAmbienteMedia': medias[2],
+            'VolumeAguaMedia': medias[3],
+        })
+    except mysql.connector.Error as err:
+        return jsonify({'error': f"Erro: {err}"}), 500
 
 @app.route("/api/mediasdata", methods=["GET"])
 def get_mediasdata():
     data = request.args.get('data')
     if not data:
         return jsonify({'error': 'Data não fornecida'}), 400
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT AVG(Temperatura), AVG(UmidadeSolo), AVG(UmidadeAmbiente), AVG(VolumeAgua) FROM dados WHERE Dia_Mes_Ano = %s", (data,))
-    medias = cursor.fetchone()
-    cursor.close()
-    if not medias or all(m is None for m in medias):
-        return jsonify({'error': 'Dados não encontrados para a data fornecida'}), 404
-    return jsonify({
-        'TemperaturaMedia': medias[0],
-        'UmidadeSoloMedia': medias[1],
-        'UmidadeAmbienteMedia': medias[2],
-        'VolumeAguaMedia': medias[3],
-    })
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+        cursor.execute("use DadosEstufa;")
+        cursor.execute("SELECT AVG(Temperatura), AVG(UmidadeSolo), AVG(UmidadeAmbiente), AVG(VolumeAgua) FROM dados WHERE Dia_Mes_Ano = %s", (data,))
+        medias = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+        if not medias or all(m is None for m in medias):
+            return jsonify({'error': 'Dados não encontrados para a data fornecida'}), 404
+        return jsonify({
+            'TemperaturaMedia': medias[0],
+            'UmidadeSoloMedia': medias[1],
+            'UmidadeAmbienteMedia': medias[2],
+            'VolumeAguaMedia': medias[3],
+        })
+    except mysql.connector.Error as err:
+        return jsonify({'error': f"Erro: {err}"}), 500
+
 if __name__ == "__main__":
-    app.run(debug=True) 
-
-
+    app.run(debug=True)
